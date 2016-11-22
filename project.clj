6e397ffa36081d9c94d46659ef6342c97a1aff20
samutamm <samutamm@gmail.com>
2016-11-22 21:18:52 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject messenger "0.1.0-SNAPSHOT"
+  :description "For slak messaging"
+  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [compojure "1.5.1"]])
