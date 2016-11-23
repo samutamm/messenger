417652ffe2380b30c2b8c1a82621bc7beb5d1288
samutamm <samutamm@gmail.com>
2016-11-23 21:28:09 +0200

    Query channels

--- src/messenger/handler.clj
+++ src/messenger/handler.clj
@@ -12,13 +12,27 @@
 
 (defn hello [] "Hello World")
 
+(def channel-query "MATCH (org:Organization)-[:HAS_CHANNEL]-(channels)
+                    WHERE org.name = {organization} return channels;")
+
+(def create-channel "MATCH (org:Organization)
+                    WHERE org.name = {organization}
+                    CREATE (c:Channel {name: {newName}}),
+                    (org)-[:HAS_CHANNEL]->(c);")
+
+(defn get-channels
+  [org]
+  (let [[result] (cy/tquery conn channel-query {:organization (str org)})]
+    result))
+
 
 (defroutes app-routes
   (GET "/" [] (resp/response (hello)))
+  (GET "/channels" [organization] (resp/response (get-channels organization)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
-
 (def app
   (-> app-routes
+      (handler/site)
       (rj/wrap-json-response)))
